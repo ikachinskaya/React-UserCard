1768a1b8386ed81050cd61367af6c19b184e7301
Inna <inna.kachinskaya13@gmail.com>
2021-09-21 22:30:07 +0300

    Refactor app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import { Component } from "react/cjs/react.production.min";
 import "./App.css";
 import data from "./data.js";
 import UserCard from "./components/UserCard";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      users: data,
-    };
-  }
-  render() {
-    return (
-      <div className="App">
-        <UserCard users={data} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <UserCard users={data} />
+    </div>
+  );
 }
 
 export default App;
