97eb2cd364e7ed1b2f5125fdf3502c3374060a22
Inna <inna.kachinskaya13@gmail.com>
2021-09-22 22:52:51 +0300

    Refactor Image

--- src/components/Image/index.jsx
+++ src/components/Image/index.jsx
@@ -1,11 +1,33 @@
 import { Component } from "react";
 
 class Image extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isError: false,
+    };
+  }
+
+  imageHandler = () => {
+    const { isError } = this.state;
+    this.setState({ isError: !isError });
+  };
+
   render() {
     const { src, alt } = this.props;
+    const { isError } = this.state;
     return (
       <div className="imgWrapper">
-        <img className="img" src={src} alt={alt} />
+        {src !== null && !isError ? (
+          <img
+            className="img"
+            src={src}
+            alt={alt}
+            onError={this.imageHandler}
+          />
+        ) : (
+          <div className="initials">Unknown</div>
+        )}
       </div>
     );
   }
